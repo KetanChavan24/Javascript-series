a48b8b8f93e02c30ff005bb1516ce4076a923e51
KetanChavan24 <111653459+KetanChavan24@users.noreply.github.com>
2023-12-18 12:14:14 +0000

    added variables file

---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountid = 12234
+let accoutnmail = "kc@gmail.com"
+var accountpassword = 1221
+accountcity = "Mumbai"
+
+accoutnmail = "ttot@gmail.com"
+accountpassword = 2234
+accountcity = "Delhi"
+let accountstate
+// ps: only use let variable
+
+console.table([accountid,accountpassword,accoutnmail,accountcity,accountstate])
