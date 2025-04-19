53068d34b2e43f6849473b391814a76763229740
KetanChavan24 <111653459+KetanChavan24@users.noreply.github.com>
2025-04-19 18:32:08 +0530

    Update 01-variables.js

--- 01_basics/01-variables.js
+++ 01_basics/01-variables.js
@@ -8,5 +8,7 @@ accountpassword = 2234
 accountcity = "Delhi"
 let accountstate
 // ps: only use let variable
-
+//cause the variable can be accessed within global scope , while let allows the variable to be accessed in the block in which it
+//is declared.
 console.table([accountid,accountpassword,accoutnmail,accountcity,accountstate])
+
