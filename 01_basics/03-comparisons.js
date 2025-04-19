9a3082b26a03049732c9a1726f853a0bb8708b65
KetanChavan24 <111653459+KetanChavan24@users.noreply.github.com>
2025-04-20 01:47:07 +0530

    Update 03-comparisons.js
    Revision

--- 01_basics/03-comparisons.js
+++ 01_basics/03-comparisons.js
@@ -6,6 +6,6 @@ console.log(null == 0);
 console.log(null >= 0);
 // The reason is that an equality check == and comparisons > < >=
 // <=work differently. Comparisons convert null to a number, treating it as 0.
-// That's why (3) null>=0 is true and (1) null0 is false.
+// That's why (null>=0) is true and  (null == 0) is false.
 
 console.log("2" === 2);//here the string is not converted into any number due to strict check(===)
