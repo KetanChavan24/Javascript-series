f5b971e09ef48be941f69e96c48a2cd777063921
KetanChavan24 <111653459+KetanChavan24@users.noreply.github.com>
2025-04-30 12:57:49 +0530

    Update 06-strings.js

--- 01_basics/06-strings.js
+++ 01_basics/06-strings.js
@@ -19,16 +19,27 @@ console.log(brandname.toLowerCase());
 //and many more...
 
 let stringname = "     Ketan     ";
-console.log(stringname);
-console.log(stringname.trim());
-console.log(stringname.trimStart());
-console.log(stringname.trimEnd());
+console.log(stringname);            // Logs: "     Ketan     "
+console.log(stringname.trim());     // Logs: "Ketan"
+console.log(stringname.trimStart()); // Logs: "Ketan     "
+console.log(stringname.trimEnd());   // Logs: "     Ketan"
+//trim removes any leading and trailing spaces 
 
-const url = "https://ketan.com/solarsytem%20cro";
-console.log(url.replace('%20','-'));
-console.log(url.includes('solar'));
+let stringname = " v    Ketan     ";
+console.log(stringname);           // Output: " v    Ketan     "
+console.log(stringname.trim());    // Output: "v    Ketan"
+console.log(stringname.trimStart()); // Output: "v    Ketan     "
+console.log(stringname.trimEnd());   // Output: " v    Ketan"
+
+
+const url = "https://ketan.com/solarsystem%20cro";
+console.log(url.replace('%20','-'));     // Output: "https://ketan.com/solarsystem-cro"
+console.log(url.includes('solar'));      // Output: false ,Returns false because the string contains "solarsytem", not "solar"
+
 
 let example = "bhavani-ketan-chavan";
-console.log(example.split('-'));// A good way of coverion of string to array
+console.log(example.split('-'));//output: [bhhavani, ketan , chavan]
+// A good way of coverion of string to array
+//split('-') breaks the string at each '-' and returns an array of parts
 
 
