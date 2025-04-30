4ee6135f7a3f344c719f24e5bd20ce0286bb80f7
KetanChavan24 <111653459+KetanChavan24@users.noreply.github.com>
2025-04-30 18:55:10 +0530

    Update 5-five.js

--- 05_iterations(loop)/5-five.js
+++ 05_iterations(loop)/5-five.js
@@ -1,24 +1,32 @@
 //FILTER
 
-// const anyNums = [1,2,3,4,5,6];
-// let myNums = anyNums.filter((num) => num > 3);
-// console.log(myNums);
-// let MyNums = anyNums.filter((num) => {
-//     return num > 2;//here return works as a scope has been formed
-// })
-// console.log(MyNums);
+const anyNums = [1,2,3,4,5,6];
+let myNums = anyNums.filter((num) => num > 3);
+console.log(myNums);//output: [4,5,6]
 
-// anyNums.forEach(element => {
-//     // if(element > 2){
-//     //     console.log(element);
-//     // }
-//     console.log(anyNums);
-// });
+let MyNums = anyNums.filter((num) => {
+     return num > 2;//here return works as a scope has been formed
+ })
+ console.log(MyNums);//output: [3,4,5,6]
 
-const newNums = [10,20,30,40,50];
+anyNums.forEach(element => {
+ if(element > 2){
+    console.log(element);
+}
+  console.log(anyNums);
+ });
+// output:[ 1, 2, 3, 4, 5, 6 ]
+//[ 1, 2, 3, 4, 5, 6 ]
+//3
+//[ 1, 2, 3, 4, 5, 6 ]
+//4
+//[ 1, 2, 3, 4, 5, 6 ]
+//5
+//[ 1, 2, 3, 4, 5, 6 ]
+//6
+//[ 1, 2, 3, 4, 5, 6 ]
 
-let myNumbers = newNums.filter((nums) => {
-return nums;
-})
 
-// console.log(myNumbers);
+
+
+
