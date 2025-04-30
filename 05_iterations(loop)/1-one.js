36f1775edaf65a2f6370313a4d8c98affc829009
KetanChavan24 <111653459+KetanChavan24@users.noreply.github.com>
2025-04-30 18:30:15 +0530

    Update 1-one.js

--- 05_iterations(loop)/1-one.js
+++ 05_iterations(loop)/1-one.js
@@ -8,22 +8,20 @@ for (let i = 0; i < 10; i++) {
 }
 
 //TABLE FOR 6
-
-for (let i = 1; i <= 10; i++) {
-    for(let j = 1; j <= 10; j++){
-        console.log(`${i} * ${j} = ${i*j}`);
-    }
-    
+for(let i = 1; i <= 10; i++){
+     console.log(i * 6);
 }
+
+
 
 //BREAK AND CONTINUE
 
 for(let k = 1; k <= 20;k++){
     if(k == 5){
         console.log(`5 detected`);
-        continue;
+        continue;// skips and jumps to next iteration
     }else if (k == 15) {
-        console.log(`15 detected`);
+        console.log(`15 detected`);// terminates the entire loop
         break;
     }
     console.log(k);
